1dee56035654ade9d61df8253e4ed8a9283f2b78
Luke Garrison <lgarriso@nd.edu>
2017-04-05 19:55:20 -0400

    Adds script to install git-completion
    For git tab-completion including branch names

---
+++ git/git.setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# install git-completion (tab-completion of branch names, etc)
+eval curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
