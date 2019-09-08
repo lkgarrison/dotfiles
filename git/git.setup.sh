fb98dc2cb4fd1c6ecbdfee4bfd531c0e068531b8
Luke Garrison <lgarriso@nd.edu>
2019-09-08 15:31:26 -0700

    Revamps dotfile setup script execution, misc vimrc and vim setup changes
    bootstrap script
    ----------------
    Automatically finds all .setup.sh files and executes them, nicely
    reporting whether each was successful or not
    
    vim/vim.setup.sh
    ----------------
    Updates vim setup script to only copy monokai colorscheme if it is
    missing to avoid unnecessary info messages, cleans up bash syntax
    
    .vimrc
    ------
    - tab changes
    - kAdds user-friendly vim colorscheme error message (can be seen when
    running bootstrap script)

--- git/git.setup.sh
+++ git/git.setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # install git-completion (tab-completion of branch names, etc)
-eval curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+eval curl -s https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
--- vim/vim.setup.sh
+++ vim/vim.setup.sh
@@ -2,8 +2,8 @@
 
 # Setup Vundle (Plugin Manager)
 vundle_repo="https://github.com/VundleVim/Vundle.vim.git"
-vundle_dir="~/.vim/bundle/"
-vundle_file="~/.vim/bundle/Vundle.vim"
+vundle_dir="$HOME/.vim/bundle/"
+vundle_file="$HOME/.vim/bundle/Vundle.vim"
 
 # Clone Vundle Plugin Manager if it wasn't already
 if [ ! -e $(eval echo $vundle_file) ]
@@ -15,25 +15,31 @@ fi
 eval "vim +PluginInstall +qall"
 
 # copy .colors files into .vim/colors/
-monokai_colors_file=${vundle_dir}"vim-monokai/colors/monokai.vim"
-vim_colors_dir="~/.vim/colors/"
+monokai_colors_file_name="monokai.vim"
+monokai_colors_source_file=${vundle_dir}"vim-monokai/colors/${monokai_colors_file_name}"
+vim_colors_dir="$HOME/.vim/colors/"
 
-# jump through hoops for tilde expansion
-if [ -e $(eval echo $monokai_colors_file) ]
+if [ ! -e $vim_colors_dir/$monokai_colors_file_name ]
 then
-	# create vim colors directory if it doesn't exist
-	if [ ! -d $(eval echo $vim_colors_dir) ]
+	if [ -e $monokai_colors_source_file ]
 	then
-		echo Making new directory: $vim_colors_dir
-		eval mkdir $vim_colors_dir
-	fi
+		# create vim colors directory if it doesn't exist
+		if [ ! -d $vim_colors_dir ]
+		then
+			echo Making new directory: $vim_colors_dir
+			eval mkdir $vim_colors_dir
+		fi
 
-	# copy monokai colors file into vim colors dir
-	echo Copying Monokai colors into proper VIM colors directory
-	eval cp "$monokai_colors_file $vim_colors_dir"
-else
-	echo ERROR: Monokai colors do not exist. Check .vimrc for correct plugin.
+		# copy monokai colors file into vim colors dir
+		echo Copying Monokai colors into proper VIM colors directory
+		eval cp "$monokai_colors_source_file $vim_colors_dir"
+	else
+		echo ERROR: Monokai colors do not exist. Check .vimrc for correct plugin.
+	fi
 fi
 
-# install flake8 for python syntax/styling checker
-eval pip install -q flake8
+# install flake8 for python syntax/styling checker if it doesn't exist
+if ! [ -x "$(command -v flake8)"  ]; then
+	echo "Installing flake8"
+	eval pip install -q --user flake8
+fi
