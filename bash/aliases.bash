3510d6ff1ef39a64a7c842c27b1acab112625694
Luke Garrison <lgarriso@nd.edu>
2019-09-09 09:08:31 -0700

    ls will display in color mode now, removes obsolete bash_aliases file

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -1,3 +1,3 @@
-if [ -f ~/.bash_aliases ]; then
-	. ~/.bash_aliases
-fi
+# ls should use colors and use special suffixes to indicate file types (i.e. * for executable)
+alias ls="ls --color=auto -F"
+alias clear='printf "\033c"'
